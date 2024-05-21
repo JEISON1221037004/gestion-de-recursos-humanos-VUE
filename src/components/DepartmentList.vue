b934f54632d926dea2ddcfa2b9b66805a4f8893d
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:24:12 -0500

    Component DepartmentList

---
+++ src/components/DepartmentList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Department List</h1>
+      <!-- Lista de departamentos irá aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DepartmentList',
+    data() {
+      return {
+        departments: []
+      };
+    },
+    methods: {
+      fetchDepartments() {
+        // Aquí se realizaría una solicitud para obtener los departamentos
+      }
+    },
+    mounted() {
+      this.fetchDepartments();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  

16ad71392a72b6d7ffe171af079cef863b3437b4
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:11:48 -0500

    Componente EmployeeList

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Employee List</h1>
+      <!-- Lista de empleados irá aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EmployeeList',
+    data() {
+      return {
+        employees: []
+      };
+    },
+    methods: {
+      fetchEmployees() {
+        // Aquí se realizaría una solicitud para obtener los empleados
+      }
+    },
+    mounted() {
+      this.fetchEmployees();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  

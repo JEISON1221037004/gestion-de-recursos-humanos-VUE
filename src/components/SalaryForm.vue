c1f630917ce875996b8329cdccd52c7f623740e9
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:30:24 -0500

    Componente SalaryForm

---
+++ src/components/SalaryForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <h1>Add New Salary</h1>
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="employee">Employee:</label>
+          <input type="text" v-model="salary.employee" id="employee" required>
+        </div>
+        <div>
+          <label for="amount">Amount:</label>
+          <input type="number" v-model="salary.amount" id="amount" required>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'SalaryForm',
+    data() {
+      return {
+        salary: {
+          employee: '',
+          amount: 0
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        axios.post('/api/salaries', this.salary)
+          .then(response => {
+            alert('Salary added successfully');
+            this.salary.employee = '';
+            this.salary.amount = 0;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos del componente */
+  </style>
+  

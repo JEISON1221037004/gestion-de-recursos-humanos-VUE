664d34e1fcb3326ac48f47326f8fb6f32931ac30
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:27:09 -0500

    Componente SalaryList

---
+++ src/components/SalaryList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Salary List</h1>
+      <!-- Lista de salarios irá aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SalaryList',
+    data() {
+      return {
+        salaries: []
+      };
+    },
+    methods: {
+      fetchSalaries() {
+        // Aquí se realizaría una solicitud para obtener los salarios
+      }
+    },
+    mounted() {
+      this.fetchSalaries();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  

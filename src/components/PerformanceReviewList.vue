336d75b8b9ed04d7a28c1347de56608ae57c9c5b
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:31:02 -0500

    Componente Performance Review List

---
+++ src/components/PerformanceReviewList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1>Performance Review List</h1>
+      <ul>
+        <li v-for="review in reviews" :key="review.id">
+          {{ review.employee }} - {{ review.comments }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'PerformanceReviewList',
+    data() {
+      return {
+        reviews: []
+      };
+    },
+    methods: {
+      fetchReviews() {
+        axios.get('/api/performance-reviews')
+          .then(response => {
+            this.reviews = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    },
+    mounted() {
+      this.fetchReviews();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos del componente */
+  </style>
+  

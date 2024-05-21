222e66e2277e25c2b3476b629a28a37846bbaa3b
JohanStiven8310 <jhanstivn93@gmail.com>
2024-05-21 12:26:32 -0500

    Componente PositionForm

---
+++ src/components/PositionForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>Position Form</h1>
+      <!-- Formulario de posición irá aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PositionForm',
+    data() {
+      return {
+        position: {}
+      };
+    },
+    methods: {
+      submitForm() {
+        // Aquí se realizaría una solicitud para enviar los datos del formulario
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
